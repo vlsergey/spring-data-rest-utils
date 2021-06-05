2ab25b3771c24bfea6ce6f540ce4d0acfaddaaa3
Sergey Vladimirov <vlsergey@gmail.com>
2021-06-05 18:44:54 +0300

    Sort keys in OpenAPI like in spec, not alphabetical (#1)

--- src/main/java/io/github/vlsergey/springdatarestutils/JacksonHelper.java
+++ src/main/java/io/github/vlsergey/springdatarestutils/JacksonHelper.java
@@ -1,28 +1,68 @@
 package io.github.vlsergey.springdatarestutils;
 
+import static java.util.Collections.singleton;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.util.DefaultIndenter;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
-import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
 import com.fasterxml.jackson.databind.ser.BeanSerializer;
 import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
 import com.fasterxml.jackson.databind.ser.PropertyWriter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter.SerializeExceptFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
-import static java.util.Collections.singleton;
-
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.media.Schema;
+import lombok.NonNull;
 import lombok.SneakyThrows;
 
 public class JacksonHelper {
 
+    // it's not strict, but nice to have fields in this particular order
+    // https://swagger.io/specification/#schema
+    private static final String[] OPENAPI_FIELDS_SPEC_ORDER = new String[] { "openapi", "info", "servers", "paths",
+	    "components", "security", "tags", "externalDocs" };
+
     private static final String SCHEMA_FILTER_ID = "SchemaFilter";
+
+    private static @NonNull JsonSerializer<?> improveOpenApiSerializer(final @NonNull BeanDescription desc,
+	    final @NonNull BeanSerializer oldSerializer) {
+	Map<String, BeanPropertyWriter> oldWriters = new LinkedHashMap<>();
+	oldSerializer.properties().forEachRemaining(w -> oldWriters.put(w.getName(), (BeanPropertyWriter) w));
+
+	// https://swagger.io/specification/#schema
+	final List<BeanPropertyWriter> newWriters = Arrays.stream(OPENAPI_FIELDS_SPEC_ORDER).map(oldWriters::remove)
+		.filter(Objects::nonNull).collect(Collectors.toCollection(ArrayList::new));
+
+	// add other items if present
+	oldWriters.values().forEach(newWriters::add);
+
+	final BeanPropertyWriter[] array = newWriters.toArray(BeanPropertyWriter[]::new);
+
+	// just because constructor is invisible
+	return new BeanSerializer(oldSerializer, array, array) {
+	    private static final long serialVersionUID = 1L;
+	};
+    }
 
     @SneakyThrows
     public static String writeValueAsString(boolean json, Object obj) {
@@ -50,8 +90,13 @@ public class JacksonHelper {
 			if (Schema.class.isAssignableFrom(desc.getBeanClass())) {
 			    return ((BeanSerializer) serializer).withFilterId(SCHEMA_FILTER_ID);
 			}
+			if (OpenAPI.class.isAssignableFrom(desc.getBeanClass())
+				&& serializer instanceof BeanSerializer) {
+			    return improveOpenApiSerializer(desc, (BeanSerializer) serializer);
+			}
 			return serializer;
 		    }
+
 		});
 	    }
 	});
