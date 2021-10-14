62483fd7874a51e2077a7b2ab5c6f8b1162f4cc7
Sergey Vladimirov <vlsergey@gmail.com>
2021-10-14 15:17:26 +0300

    Support Collection as parameter and as a result of finder, also Page as result

--- src/test/java/io/github/vlsergey/springdatarestutils/customfinders/TestEntityRepo.java
+++ src/test/java/io/github/vlsergey/springdatarestutils/customfinders/TestEntityRepo.java
@@ -2,6 +2,7 @@ package io.github.vlsergey.springdatarestutils.customfinders;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
@@ -15,6 +16,12 @@ public interface TestEntityRepo extends JpaRepository<TestEntity, UUID> {
     Page<TestEntity> findAll(Pageable pageable);
 
     List<TestEntity> findByValue(int value);
+
+    @Query("SELECT t FROM TestEntity t WHERE value=:value")
+    List<TestEntity> findListByValue(int value);
+
+    @Query("SELECT t FROM TestEntity t WHERE value IN (:value)")
+    List<TestEntity> findListByValuesSet(Set<Integer> value);
 
     @Query("SELECT t FROM TestEntity t WHERE value=?1")
     TestEntity findOneSimple(int value);
